79051f798947a9d6cce5e1aa1f2d88b30a0026cf
Kai Quander <kq1003@gmail.com>
2020-07-02 12:45:43 -0400

    new changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import foods from './foods.json';
 import FoodBox from './components/Foodbox';
+import 'bulma/css/bulma.css';
 
 class App extends Component {
   state = {
